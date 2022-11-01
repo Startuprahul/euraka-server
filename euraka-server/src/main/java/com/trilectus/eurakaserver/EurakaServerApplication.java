9a24aad5f8c0de98c76f36e12dbf20981cadd49d
Startuprahul <117197254+Startuprahul@users.noreply.github.com>
2022-11-01 21:50:32 +0530

    Initial commit

---
+++ euraka-server/src/main/java/com/trilectus/eurakaserver/EurakaServerApplication.java
@@ -0,0 +1,13 @@
+package com.trilectus.eurakaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakaServerApplication.class, args);
+	}
+
+}
---
+++ euraka-server/src/test/java/com/trilectus/eurakaserver/EurakaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilectus.eurakaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
